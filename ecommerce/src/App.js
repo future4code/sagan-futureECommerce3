f6f0027bfe9e32774bfacda7b31b213301d2ea61
Andrius <andrius.rochalazarino@gmail.com>
2020-02-20 08:42:47 -0400

    adicionado lista com os produtos

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Aplicativo = styled.div`
+
+`
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+           listaProdutos: [
+             {nome:"Camiseta perdido no espaço", preco: 29.90, imagem:"./img/prod1.png"},
+             {nome:"Camiseta rebelião no espaço", preco: 23.60, imagem:"./img/prod2.png"},
+             {nome:"Camiseta onibus espacial feminina", preco: 36.30, imagem:"./img/prod3.png"},
+             {nome:"Camiseta astrodev", preco: 39.30, imagem:"./img/prod4.png"},
+             {nome:"Camiseta naza feminina", preco: 16.60, imagem:"./img/prod5.jpg"},
+             {nome:"Camiseta miau espacial", preco: 29.90, imagem:"./img/prod6.png"},
+             {nome:"Camiseta leve-me por favor", preco: 26.60, imagem:"./img/prod7.png"},
+             {nome:"Camiseta tom and jerry", preco: 46.30, imagem:"./img/prod8.png"}
+            ]
+        }
+    }
+
+
+     teste = ()=>{
+      const teste = this.state.listaProdutos.map((elemento,index,array)=>{
+        return (<div key={index}>{elemento.nome}</div>)
+        })
+        console.log(teste)
+        return teste
+    }
+    render() {
+
+        return (
+            <Aplicativo>
+                <div>{this.teste()}</div>
+            </Aplicativo>
+        )
+    }
 }
 
-export default App;
+export default App
