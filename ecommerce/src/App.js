bd736404de740a2fbd6e342a16d8dfb6ef26a0d3
Andrius <andrius.rochalazarino@gmail.com>
2020-02-20 09:49:43 -0400

    adicionado rascunho componente produto

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import styled from 'styled-components';
+import Produto from './Components/Produto';
 
 const Aplicativo = styled.div`
 
@@ -12,14 +12,14 @@ class App extends React.Component {
         super(props)
         this.state = {
            listaProdutos: [
-             {nome:"Camiseta perdido no espaço", preco: 29.90, imagem:"./img/prod1.png"},
-             {nome:"Camiseta rebelião no espaço", preco: 23.60, imagem:"./img/prod2.png"},
-             {nome:"Camiseta onibus espacial feminina", preco: 36.30, imagem:"./img/prod3.png"},
-             {nome:"Camiseta astrodev", preco: 39.30, imagem:"./img/prod4.png"},
-             {nome:"Camiseta naza feminina", preco: 16.60, imagem:"./img/prod5.jpg"},
-             {nome:"Camiseta miau espacial", preco: 29.90, imagem:"./img/prod6.png"},
-             {nome:"Camiseta leve-me por favor", preco: 26.60, imagem:"./img/prod7.png"},
-             {nome:"Camiseta tom and jerry", preco: 46.30, imagem:"./img/prod8.png"}
+             {id:1, nome:"Camiseta perdido no espaço", preco: 29.90, imagem:require("./img/prod1.png")},
+             {id:2, nome:"Camiseta rebelião no espaço", preco: 23.60, imagem:require("./img/prod2.png")},
+             {id:3, nome:"Camiseta onibus espacial feminina", preco: 36.30, imagem:require("./img/prod3.png")},
+             {id:4, nome:"Camiseta astrodev", preco: 39.30, imagem:require("./img/prod4.png")},
+             {id:5, nome:"Camiseta naza feminina", preco: 16.60, imagem:require("./img/prod5.jpg")},
+             {id:6, nome:"Camiseta miau espacial", preco: 29.90, imagem:require("./img/prod6.png")},
+             {id:7, nome:"Camiseta leve-me por favor", preco: 26.60, imagem:require("./img/prod7.png")},
+             {id:8, nome:"Camiseta tom and jerry", preco: 46.30, imagem:require("./img/prod8.png")}
             ]
         }
     }
@@ -27,7 +27,7 @@ class App extends React.Component {
 
      teste = ()=>{
       const teste = this.state.listaProdutos.map((elemento,index,array)=>{
-        return (<div key={index}>{elemento.nome}</div>)
+        return (<Produto key={elemento.id} nome={elemento.nome} imagem={elemento.imagem} preco={elemento.preco} />)
         })
         console.log(teste)
         return teste
